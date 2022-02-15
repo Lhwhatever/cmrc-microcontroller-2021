6903ef729de633a7c02341fb82207921e6256216
hoffmanjc13 <hoffmanjc13@gmail.com>
2022-02-15 16:06:46 -0500

    created test for stepper under PI control using imu

---
+++ steppertest.py
@@ -0,0 +1,44 @@
+import stepper
+import time
+import board
+import adafruit_bno055
+import busio
+
+kP = 10
+kI = 0.5
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_bno055.BNO055_I2C(i2c)
+motor = stepper.Stepper(23, 13, 15, 19, 21)
+motor.set_resolution("1/2")
+motor.start_motor()
+
+t0 = time.time()
+i = 0
+while True:
+    # get the gravity vector from the board
+    grav = sensor.gravity
+    t = time.time()
+
+    # find the error by:
+    # - removing all z component (which we can't fix)
+    # - normalizing the vec
+    # - dotting it with [0, -1, 0]
+
+    gravLen = (grav[0]**2 + grav[1]**2)**.5
+    normalGravY = grav[1]/gravLen
+    error = -normalGravY
+
+    # now to run the PI controller
+    # yummm pie
+    i += error*(t - t0)
+
+    controlOut = kP*error + kI*i
+
+    # motor time bois!!
+    # each step should take 0.05 sec
+    motorSteps = abs(controlOut)
+    stepDir = controlOut//motorSteps
+    stepFreq = motorSteps/0.05
+
+    motor.move_motor(motorSteps, stepDir, stepFreq)
